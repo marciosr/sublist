00e48388041818379aa8078bc92b50f88a07f2a4
Márcio <marcio@archnote.localdomain>
2022-04-23 19:21:57 -0300

    Initial changes

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,19 @@ pub fn sublist<T: PartialEq>(
 		Comparison::Equal
 	} else if first_list == &[] && second_list != &[] {
 		Comparison::Sublist
-	} else if {
-	
+	} else if first_list != &[] && second_list == &[] {
+		Comparison::Superlist
+	} else if first_list == second_list {
+		Comparison::Equal
+	} else if first_list.len() < second_list.len() {
+		Comparison::Unequal ///TODO: to include the comparisson function here!
+ 	} else {
+		Comparison::Unequal ///TODO: to include the comparisson function here!
 	}
+}
 
+fn compare<T: PartialEq>( small_list: &[T], bigger_list: &[T] ) -> bool {
+	let small_size: usize = small_list.len();
+	let mut slice = small_list.chunks_exact(small_size);
 
-	Comparison::Equal
 }
