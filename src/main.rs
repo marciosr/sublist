c8843ed95d7dbd838307c39e134e63b91638aae4
Márcio <marcio@archdesk.localdomain>
2022-05-01 12:42:02 -0300

    Change functions to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,12 @@
 use sublist::{sublist, Comparison};
 
 fn main () {
-		
-}
 
-// #[derive(Debug, PartialEq)]
-// pub enum Comparison {
-//     Equal,
-//     Sublist,
-//     Superlist,
-//     Unequal,
-// }
-
-pub fn sublist<T: PartialEq>(
-	first_list: &[T],
-	second_list: &[T] 
-) -> Comparison {
-
-	//let res = first_list.eq(second_list);
-		
-	if first_list == &[] && second_list == &[] {
-		Comparison::Equal
-	} else if first_list == &[] && second_list != &[] {
-		Comparison::Sublist
-	} else if first_list != &[] && second_list == &[] {
-		Comparison::Superlist
-	} else if first_list == second_list {
-		Comparison::Equal
-	} else if first_list.len() < second_list.len() {
-		Comparison::Unequal ///TODO: to include the comparisson function here!
- 	} else {
-		Comparison::Unequal ///TODO: to include the comparisson function here!
+	match sublist(&[1, 1, 2], &[1, 1, 1, 2]) {
+		Comparison::Equal => println!("Iguais"),
+		Comparison::Sublist => println!("Sublista"),
+		Comparison::Superlist => println!("Superlista"),
+		Comparison::Unequal => println!("Diferentes"),
 	}
 }
 
-fn compare<T: PartialEq>( small_list: &[T], bigger_list: &[T] ) -> bool {
-	let small_size: usize = small_list.len();
-	let mut slice = small_list.chunks_exact(small_size);
-
-}
